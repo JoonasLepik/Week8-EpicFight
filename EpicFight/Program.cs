b0ac531ad3f05ef3649d18bff387e3fda68e45d9
JoonasLepik <89841616+JoonasLepik@users.noreply.github.com>
2021-10-26 13:41:41 +0300

    Update Program.cs

--- EpicFight/Program.cs
+++ EpicFight/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace EpicFight
 {
@@ -11,9 +11,24 @@ namespace EpicFight
             string heroWeapon = RandomWepons();
             string villainWepon = RandomWepons();
             villain = RandomVillain();
-            Console.WriteLine($"{hero} will fight {villain}.");
+            int heroHP = GenerateHP(hero);
+            int villainHP = GenerateHP(villain);
+            Console.WriteLine($"{hero} ({heroHP}) will fight {villain} ({villainHP}).");
             Console.WriteLine($"{hero} will fight with {heroWeapon}.");
             Console.WriteLine($"{villain} will fight with {villainWepon}.");
+            while (heroHP > 0 && villainHP > 0)
+            {
+                heroHP = heroHP - Hitpoints(villain, hero, villainWepon);
+                villainHP = villainHP - Hitpoints(hero, villain, heroWeapon);
+            }
+            if (heroHP <= 0)
+            {
+                Console.WriteLine("The Bad guys has won!");
+            }
+            else
+            {
+                Console.WriteLine($"{hero} lives to see another day!");
+            }
         }
         private static int RandomIndex(string[] someArrays)
         {
@@ -37,9 +52,33 @@ namespace EpicFight
         }
         private static string RandomWepons()
         {
-            string[] wepons = { "Tech X", "Brimstrone", "Gun", "Pipis", "Devel's Knife", "HeadCannon", "Dice", "Cards" };
+            string[] wepons = { "Tech X", "Brimstrone", "Revolver", "Pipis", "Devel's Knife", "HeadCannon", "Dice", "Cards" };
             return wepons[RandomIndex(wepons)];
         }
+        private static int GenerateHP(string someCharater)
+        {
+            Random rnd = new Random();
+            return rnd.Next(someCharater.Length, someCharater.Length + 10);
+        }
+        private static int Hitpoints(string CharacterOne, string characterTwo, string wepons)
+        {
+            Random rnd = new Random();
+            int strike = rnd.Next(0, wepons.Length / 2);
+            Console.WriteLine($"{CharacterOne} hit {strike} on {characterTwo}.");
+            if(strike == wepons.Length/2 - 1)
+            {
+                Console.WriteLine($"Awesome! {CharacterOne} made a critical hit!");
+            }
+            else if (strike == 0)
+            {
+                Console.WriteLine($"{characterTwo} has dodged the attack!");
+            }
+            return strike;
+        }
+       
+        
+
+        
 
     }
 
